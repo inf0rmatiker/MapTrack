40bca9212f66216b4be3e0fee7d3dbbe20b23dae
Caleb Carlson <ccarlson355@gmail.com>
2020-03-12 20:55:14 -0600

    Add note about google api key

--- src/components/Application/Application.js
+++ src/components/Application/Application.js
@@ -4,7 +4,19 @@ import { getApiKey } from '../../GoogleApiKey';
 import {Card, CardBody, CardHeader, Container} from 'reactstrap';
 import GoogleMapReact from 'google-map-react';
 
+/* 
+  Maintains the body of the application. Header and footer should
+  not be included here.
 
+  --- NOTE ---
+  For confidentiality, the google maps API key has not been
+  included in the public repository.
+
+  To use this with Google, please provide your own Google API
+  key in a file: src/GoogleApiKey.js, which contains only:
+
+    export function getApiKey() { return <YOUR_KEY>; }
+*/
 export default class Application extends Component {
 
   constructor(props) {
